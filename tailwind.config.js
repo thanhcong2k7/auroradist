3ca84b237bac396270ec795ce1b76903c3dffccf
thanhcong2k7 <61042760+thanhcong2k7@users.noreply.github.com>
2026-01-10 00:29:08 +0700

    PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         brand: {
+          // Sử dụng biến CSS đã khai báo ở index.css
           primary: 'rgb(var(--brand-primary) / <alpha-value>)',
           secondary: 'rgb(var(--brand-secondary) / <alpha-value>)',
-          bg: '#050505', 
+          bg: 'rgb(var(--brand-bg) / <alpha-value>)',
           surface: '#111111',
         }
       }
